0def40b377e2ab998dff4e094e0bf622f18a3bd7
conzaa <cons.mendoza.m@gmail.com>
2024-07-29 22:10:55 -0400

    Primer Ejercicio

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKW41Z_QH23f7slD5izsnN_L8ZAKTZzK4",
+  authDomain: "mp-proyecto-a5abf.firebaseapp.com",
+  projectId: "mp-proyecto-a5abf",
+  storageBucket: "mp-proyecto-a5abf.appspot.com",
+  messagingSenderId: "569760419885",
+  appId: "1:569760419885:web:e43a74ab38b685714868a4",
+  measurementId: "G-PWYG8362Q0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="addUsuarios">
+      <input v-model="nuevoUsuarios" placeholder="Nuevo Usuarios" />
+      <button type="submit">Agregar</button>
+    </form>
+    <ul>
+      <li v-for="Usuario of Usuarios" :key="Usuario.id">
+        {{ Usuario.id }} - {{ Usuario.nombre }} - {{ Usuario.correo }}
+        <button @click="deleteUsuarios(Usuario.id)">Eliminar</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { getFirestore, collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore';
+import App from '../config/firebase';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      nuevoUsuarios: '',
+      Usuarios: [],
+    }
+  },
+
+  mounted() {
+    const db = getFirestore(App);
+    const UsuariosRef = collection(db, 'Usuarios');
+    // "todos" es el nombre de la colección en Firestore
+    onSnapshot(UsuariosRef, (snapshot) => {
+      this.Usuarios = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    });
+  },
+  methods: {
+    async addUsuarios() {
+      if (this.nuevoUsuarios.trim() === ''){
+        alert("no has escrito nada")
+        return;
+      }
+
+      if (this.nuevoUsuarios.length <3 ){
+        alert(" mas de 3 caracteres")
+        return;
+      }
+      
+      const db = getFirestore(App);
+      const UsuariosRef = collection(db, 'Usuarios');
+      
+      await addDoc(UsuariosRef, { nombre: this.nuevoUsuarios });
+      this.nuevoUsuarios = ''; // Limpiar el campo después de agregar },
+
+ 
+    },
+       
+    async deleteUsuarios(UsuariosId) {
+      const db = getFirestore(App);
+      const UsuariosRef = doc(db, 'Usuarios', UsuariosId);
+
+      await deleteDoc(UsuariosRef);
+    }
   }
-}
+};
 </script>
+
+<style></style>
